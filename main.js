be094a2a377c8c1af24fca5d5162a3b471f9fcc2
Aymen07171 <elattarayman1@gmail.com>
2023-08-13 21:38:45 +0100

    Upload All main File

---
+++ main.js
@@ -0,0 +1,9 @@
+function toggleList() {
+    const listElement = document.getElementById("list");
+    listElement.classList.toggle("hidden");
+  }
+  
+  function hideList() {
+    const listElement = document.getElementById("list");
+    listElement.classList.add("hidden");
+  }
